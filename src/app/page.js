2dd377e90489d5c681e9ed8695ade6507f6052db
Ty Streets <tstreets@iu.edu>
2024-10-23 11:42:30 -0400

    Pulling in total count of pokemon

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,17 @@
+"use client";
+import usePokemonApi from "@/hooks/usePokemonApi";
+import { useEffect } from "react";
+
 export default function Home() {
+  const pokeData = usePokemonApi();
+
+  useEffect(() => {
+    if (pokeData.totalPokemonCount === 0) {
+      pokeData.getNumberOfPokemon();
+    }
+  }, [pokeData]);
+
+  console.log(pokeData);
   return (
     <main>
       <h1>Home</h1>
